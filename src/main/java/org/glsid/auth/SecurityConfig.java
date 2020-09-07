8469138e714e8598334ba818a5638cca33008446
MAAZAZ <zakitche@gmail.com>
2020-09-07 10:05:18 +0100

    update

---
+++ src/main/java/org/glsid/auth/SecurityConfig.java
@@ -0,0 +1,26 @@
+package org.glsid.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+//@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors();
+		http.csrf().disable();
+		http.formLogin();
+		http.authorizeRequests().antMatchers("/**").fullyAuthenticated().and().httpBasic();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("zakaria").password("{noop}1234").roles("User");
+	}
+	
+}
